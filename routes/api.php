e15358ae39b82c1568242202f8efeea63b019d65
Taufik Ridho <taufikridho505@gmail.com>
2025-01-09 13:45:45 +0700

    add: Create & Delete Publisher

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\PublisherController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('publishers')->group(function () {
+    Route::get('/', [PublisherController::class, 'index']);
+    Route::post('/', [PublisherController::class, 'store']);
+    Route::delete('/{publisher}', [PublisherController::class, 'destroy']);
+});
